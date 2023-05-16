6c3947af338b243ba6b3ade2e33c99519f26f9d2
abdorizk49 <abdorizk49@gmail.com>
2023-05-16 12:35:07 +0300

    16 may

--- src/pages/ProductDetailsPage.js
+++ src/pages/ProductDetailsPage.js
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 import {products} from "../api/API"
 
 function ProductDetailsPage(){
-    const api_url = "https://646345827a9eead6fae1a476.mockapi.io/products";
+    // const api_url = "https://646345827a9eead6fae1a476.mockapi.io/products";
     const [products, setProducts] = useState([]);
     const {productId} = useParams();
     const getProduct = () => {

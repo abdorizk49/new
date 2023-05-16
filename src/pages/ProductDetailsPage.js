2d37f431c8ee3b9b6e2a6123ca3ed44948f93dde
abdorizk49 <abdorizk49@gmail.com>
2023-05-16 12:16:35 +0300

    16 may

--- src/pages/ProductDetailsPage.js
+++ src/pages/ProductDetailsPage.js
@@ -1,41 +1,22 @@
 import { useEffect, useState } from "react";
 import ProductDetails from "../components/ProductDetails";
-// import { useParams } from "react-router-dom";
-import "../api/Data.json"
+import { useParams } from "react-router-dom";
 
-function ProductDetailsPage({productId}){
-    // const api_url = "../api/Data.json/products";
-    // const [product, setProduct] = useState([]);
-    const [product, setProduct] = useState(null);
-    // const {productId} = useParams();
-    // const getProduct = () => {
-    //     fetch(`${api_url}/${productId}`)
-    //     .then((res) => res.json())
-    //     .then((data) => setProduct(data));
-    // }
+function ProductDetailsPage(){
+    const api_url = "https://646345827a9eead6fae1a476.mockapi.io/products";
+    const [product, setProduct] = useState([]);
+    const {productId} = useParams();
+    const getProduct = () => {
+        fetch(`${api_url}/${productId}`)
+        .then((res) => res.json())
+        .then((data) => setProduct(data));
+    }
     
-
-    // useEffect(() => {
-    //     getProduct();
-    // }, []);
 
     useEffect(() => {
-        const fetchProduct = async () => {
-          try {
-            // Simulating fetching data from a local file
-            const response = await fetch('../api/Data.json');
-            const data = await response.json();
-            console.log(response);
-            // Find the product with the matching ID
-            const product = data.products.find((p) => p.id === productId);
-            setProduct(product);
-          } catch (error) {
-            console.error('Error fetching product:', error);
-          }
-        };
-    
-        fetchProduct();
-      }, [productId]);
+        getProduct();
+    }, []);
+
 
     return(
         <>
